dc4f5939c76a8f52382d7023415e9546df852453
Jharell Chambers <jharell.chambers@selu.edu>
2019-08-13 06:18:54 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -2,9 +2,12 @@ def create_an_empty_array
   array = []
 end
 #----------------------------------------------------------------
+
 def create_an_array
   array = [1,2,3,4]
 end
+
+#----------------------------------------------------------------
 
 def add_element_to_end_of_array(array, element)
  old_array = array
@@ -12,7 +15,7 @@ def add_element_to_end_of_array(array, element)
  end
   
   add_element_to_end_of_array([1,2,3,4], 5)
-
+#----------------------------------------------------------------
 
 def add_element_to_start_of_array(array, element)
   

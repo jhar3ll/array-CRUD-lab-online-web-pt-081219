4096ba301892b36fabeb324c71272e4799f162ce
Jharell Chambers <jharell.chambers@selu.edu>
2019-08-13 06:19:55 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -10,23 +10,24 @@ end
 #----------------------------------------------------------------
 
 def add_element_to_end_of_array(array, element)
- old_array = array
- element << array
+ array.push(element)
  end
   
   add_element_to_end_of_array([1,2,3,4], 5)
 #----------------------------------------------------------------
 
 def add_element_to_start_of_array(array, element)
-  
+  array.unshift(element)
 end
 
  add_element_to_start_of_array([1,2,3,4], 5)
 
+#----------------------------------------------------------------
+
 def remove_element_from_end_of_array(array)
   
 end
-
+#----------------------------------------------------------------
 def remove_element_from_start_of_array(array)
   
 end
